1bf9bd305ef58065c07bd8fab8808cc192cada1f
GuillermoB <213455018+88guilleb@users.noreply.github.com>
2025-05-28 14:09:13 -0300

    Mi primer commit

---
+++ app.js
@@ -0,0 +1,6 @@
+// Espera que el contenido de la página esté completamente cargado
+document.addEventListener("DOMContentLoaded", () => {
+  const saludo = document.getElementById("saludo");
+  saludo.textContent = "¡Hola desde JavaScript!";
+});
+
